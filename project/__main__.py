6923f1f969877602de9c1035a607b7e3be7d2dd4
Jasmine <obviously0utraged@yahoo.com>
2018-04-18 22:47:19 -0400

    Imported gmplot and added its library from Github (puts it back)

--- project/__main__.py
+++ project/__main__.py
@@ -1,41 +1,38 @@
-import numpy as np
 import pandas as pd
 import time
-import os
-# import pycuda.driver as cuda
-# import pycuda.autoinit
-# from pycuda.compiler import SourceModule
-# from gmplot import gmplot
+import pycuda.driver as cuda
+import pycuda.autoinit
+from pycuda.compiler import SourceModule
+import numpy as np
+from gmplot import gmplot
 
-
+"""TODO: Write the severity algorithm given by Dr. C and use
+allocate function to send data there"""
 def scoring(killed, injured, score):
-    # TODO: Write the severity algorithm given by Dr. C and use allocate function to send data there
     """Send the lists to the gpu, score each row, return lists back to CPU"""
-    # mod = SourceModule("""
-    # __global__ void scoring(
-    #
-    # """)
+    mod = SourceModule("""
+    __global__ void scoring(
+    
+    """)
 
-
+"""TODO: send a numpy/cuda array to the gpu and allocate
+blocks within the gpu for it, used by scoring function"""
 def allocate_send_data_gpu(np_array):
-    # TODO: send a numpy/cuda array to the gpu and allocate blocks within the gpu for it, used by scoring function
+
     """Adds a severity score column to our csv data frame"""
-
-
 def add_column(data_df):
     null_data = []
     data_df['Severity Score'] = null_data
     return data_df
 
-
+"""Imports the NYPD collisions csv and returns a data frame
+for manipulation"""
 def import_csv():
-    """Imports the NYPD collisions csv and returns a data frame
-    for manipulation"""
-    data_df = pd.read_csv(os.path.join(os.path.dirname(__file__)), './data/NYPD_Motor_Vehicle_Collisions.csv')
+    data_df = pd.read_csv('NYPD_Motor_Vehicle_Collisions.csv')
     return data_df
 
-
 def main():
+
     # get the start time
     start_time = time.time()
 
@@ -63,6 +60,5 @@ def main():
 
     # score the cuda arrays using scoring function
     data_df = scoring(cuda_killed, cuda_injured, cuda_score)
-
 
 main()
