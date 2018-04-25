3e6fe7a0b2efd2c71b2a1f01e9ed3eb16ba6003e
jhern08 <kidjose1998@gmail.com>
2018-04-25 11:08:03 -0400

    Updated module. Program still stops running there.

--- project/CudaPlot.py
+++ project/CudaPlot.py
@@ -4,10 +4,21 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
 import numpy
+import pandas
+
+
+
+#import __main__
+
+
 
 class CudaPlotter(gmplot.GoogleMapPlotter):
+
+     # def __init__(self, center_lat, center_lng, zoom, data_frame, apikey=''):
+     # 	 gmplot.GoogleMapPlotter.__init__(self, center_lat, center_lng, zoom, apikey='')
+     #	 self.df = data_frame	
      
-      def heatmap(self, lats, lngs, threshold=10, radius=10, gradient=None, opacity=0.6, maxIntensity=1, dissipating=True):
+      def heatmap_GPU(self, lats, lngs, threshold=10, radius=10, gradient=None, opacity=0.6, maxIntensity=1, dissipating=True):
          """
          :param lats: list of latitudes
          :param lngs: list of longitudes
@@ -27,52 +38,60 @@ class CudaPlotter(gmplot.GoogleMapPlotter):
          settings['maxIntensity'] = maxIntensity
          settings['dissipating'] = dissipating
          settings = self._process_heatmap_kwargs(settings)
-	 
-
-	 #Sequential CPU Implementation  
-         #heatmap_points = []
-         #for lat, lng in zip(lats, lngs):
-         #   heatmap_points.append((lat, lng))
-
-	  
+ 
+      
+         heatmap_points = []
+ 
+	 heatmap_points = numpy.array(heatmap_points)
+	 lats = numpy.array(lats)
+	 lngs = numpy.array(lngs)
 	 #GPU Implementation
 	 #Change lists to Numpy Arrays 
          heatmap_points = heatmap_points.astype(numpy.float32)
 	 tempLats = lats.astype(numpy.float32)
 	 tempLngs = lngs.astype(numpy.float32)
-	
+         
+	 print("works here")
+
+	 		
 	 #Allocate memory in GPU  
 	 lats_gpu = cuda.mem_alloc(tempLats.nbytes)
 	 lngs_gpu = cuda.mem_alloc(tempLngs.nbytes)
-	 points_gpu = cuda.mem_alloc(heatmap_points.nbytes)
+	 points_gpu = cuda.mem_alloc(tempLats.nbytes*2)
 
+	 print ("works here 2")
 	 #Transfer data from CPU(Host) to GPU(Device) 
 	 cuda.memcpy_htod(lats_gpu, tempLats)
 	 cuda.memcpy_htod(lngs_gpu, tempLngs)
-	 cuda.memcpy_htod(points_gpu, heatmap_points)
+	 #cuda.memcpy_htod(points_gpu, heatmap_points)
 
+	 N = tempLats.nbytes
+	 print("works here 3")
 	 #C Implementation of commented out code above
-	 heatmapGPU = SourceModule (
-	 """
-	    __global__ void heatmapGPU(float *lats, float *lngs, float *heatmap_points)
+	 heatmapGPU = pycuda.compiler.SourceModule("""
+	    __global__ void heatmapGPU(float *lats, float *lngs, float **heatmap_points)
 	    {
-		int idx = 0;
-		int idy = 0;
-		heatmap_points[idx][0] = lats[idx]; #Column 0 is for lats values 
-		heatmpa_points[idy][1] = lngs[idy]; #Column 1 is for lngs values
-
+		int idx = blockIdx.x;
+		int idy = blockIdy.y;
+	        if (idx < N)
+			heatmap_points[idx][0] = lats[idx]; #Column 0 is for lats values
+		if (idy < N) 
+			heatmpa_points[idy][1] = lngs[idy]; #Column 1 is for lngs values
+		printf("works here in module")
 	    }	
-	 """		
-	 )
+	    """)
 	
+  	 print("works here 4")
  	 #Execute kernel function using GPU versions of the "Lists" as args
          func = mod.get_function("heatmapGPU")
 	 func(lats_gpu, lngs_gpu, points_gpu, block = (400,1,1))
 
+	 print("works here 5")
 	 #Bring back data to the CPU from temp var and into constructor variable heatmap_points
 	 tempPoints = numpty.empty_like(heatmaps_points)
 	 cuda.memcpy_dtoh(tempPoints, points_gpu)
 
+	 print("works here 6")
 	 #Convert back to list for following line
 	 heatmaps_points = tempPoints.tolist()
 
@@ -102,12 +121,16 @@ class CudaPlotter(gmplot.GoogleMapPlotter):
 
 if __name__ == '__main__':
 
-     #gmap = CudaPlot.gmplot.GoogleMapPlotter.from_geocode("San Francisco")
-     # Place map
-     gmap = CudaPlotter(37.766956, -122.438481, 13)
-
+     # Test code for checking to see if results are the same or different from the parent class
      
+     gmap = gmplot.GoogleMapPlotter(37.766956, -122.438481, 13)
+     cudamap = CudaPlotter(37.766956, -122.438481, 13)	
 
+     path4 = [(37.433302 , 37.431257 , 37.427644 , 37.430303), (-122.14488, -122.133121, -122.137799, -122.148743)]
+     
+     #Both Heatmap function require Python List type in the args	
+     gmap.heatmap(path4[0], path4[1], threshold=10, radius=40, dissipating=False)
+     cudamap.heatmap_GPU(path4[0], path4[1], threshold=10, radius=40, dissipating=False)	
      # Draw
-     gmap.draw("my_map.html")
-     #gmap.draw("my_map.html")
+     gmap.draw("gmplot_map.html")
+     cudamap.draw("cuda_plot.html")	    
