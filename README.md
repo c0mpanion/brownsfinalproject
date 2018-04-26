59242ea9e31e278293f5f29a620c2845387173cb
Marcos Castaneda <marcosjcastaneda@gmail.com>
2018-04-26 00:03:38 -0400

    Adds more efficient parallel strategy, adds thread count parameter for command args

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 2. `./install`
 3. `python project`
 
-### Notes
+## Usage
+### Specify Thread Count
+- `python project -threads 64`
+- `python project -t 64`
+
+## Notes
 1. Make sure your `~/.bashrc` has a path to Cuda/nvcc `setenv PATH ${PATH}:/usr/local/cuda/bin` then run `source ~/.bashrc`
--- project/__main__.py
+++ project/__main__.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 import os
 import sys
+import argparse
 
 from LinearStrategy import LinearStrategy
 from ParallelStrategy import ParallelStrategy
@@ -28,10 +29,20 @@ def import_csv():
     return data_frame
 
 
+def parse_args():
+    """Parse arguments from the command line"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "-threads", help="specifies a thread count for parallel operations", type=int)
+    return parser.parse_args()
+
+
 def main():
     print("Python version: " + sys.version)
     print("Pandas version: " + pd.__version__)
     print("numpy version: " + np.__version__)
+
+    # Parse command line arguments
+    args = parse_args()
 
     # get the start time
     start_time = time.time()
@@ -44,28 +55,8 @@ def main():
 
     # Run strategies
     ls = LinearStrategy(df_collisions)
-    ps = ParallelStrategy(df_collisions)
+    ps = ParallelStrategy(df_collisions, args.t)
     cs = CudaStrategy(df_collisions)
-
-    # ['NUMBER OF PEDESTRIANS INJURED'].mean()
-    # print(mean)
-    # collisions.describe(include="all")
-
-    # add column for scoring
-    # data_frame = add_column(df_collisions)
-
-    # pulling needed columns from data frame for severity score
-    # df_killed = df_collisions['NUMBER OF PERSONS KILLED'].tolist()
-    # df_injured = df_collisions['NUMBER OF PERSONS INJURED'].tolist()
-    # df_score = data_frame['Severity Score'].tolist()
-
-    # converts the column lists to cuda arrays
-    # array_killed = np.array(df_killed, dtype=np.int32)
-    # array_injured = np.array(df_injured, dtype=np.int32)
-    # array_score = np.array(df_score, dtype=np.int32)
-
-    # score the cuda arrays using scoring function
-    # data_frame = scoring(cuda_killed, cuda_injured, cuda_score)
 
 
 main()
